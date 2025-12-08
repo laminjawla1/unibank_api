b87080331ea4a0c398f9c94b57e9443ccb2af6b4
Lamin Jawla <laminjawla1001@gmail.com>
2025-12-08 09:15:39 +0000

    first commit

---
+++ src/test/java/com/unibank/api/UnibankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
