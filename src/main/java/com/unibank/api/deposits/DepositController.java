420ad5943931e3c3a813ad84c32624e1bbcacc0c
Lamin Jawla <laminjawla1001@gmail.com>
2026-01-05 08:47:34 +0000

    Deposit and withdrawal package removed

--- src/main/java/com/unibank/api/deposits/DepositController.java
+++
@@ -1,7 +0,0 @@
-package com.unibank.api.deposits;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class DepositController {
-}
--- src/main/java/com/unibank/api/withdrawals/WithdrawalController.java
+++
@@ -1,7 +0,0 @@
-package com.unibank.api.withdrawals;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class WithdrawalController {
-}
